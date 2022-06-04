0aa32b8d18bd8941282a51582a1128a77c0582ab
Mathew Winters <mathew@winters.org.nz>
2022-06-04 14:08:44 +1200

    Initial Commit new repository

---
+++ src/module.ts
@@ -0,0 +1,7 @@
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { SimplePanel } from './SimplePanel';
+
+export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
+  return builder;
+});
